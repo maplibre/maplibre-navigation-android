268a2cc4af2b2aca6ad617532e4a4d892d557cc7
Fabian Keunecke <github@fabiankeunecke.de>
2023-09-21 12:42:39 +0200

    Clean whitespaces

--- app/src/main/java/com/mapbox/services/android/navigation/testapp/NavigationUIActivity.kt
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/NavigationUIActivity.kt
@@ -71,7 +71,7 @@ class NavigationUIActivity :
                 val options = NavigationLauncherOptions.builder()
                     .directionsRoute(route)
                     .shouldSimulateRoute(simulateRoute)
-                    .initialMapCameraPosition(CameraPosition.Builder().target(LatLng( userLocation.latitude,userLocation.longitude)).build())
+                    .initialMapCameraPosition(CameraPosition.Builder().target(LatLng(userLocation.latitude, userLocation.longitude)).build())
                     .lightThemeResId(R.style.TestNavigationViewLight)
                     .darkThemeResId(R.style.TestNavigationViewDark)
                     .build()
