554dd97bc0339c330a16bd980e9f3d93ddfa9192
Fabian Keunecke <f@biankeunecke.de>
2024-03-19 13:44:56 +0100

    Remove unused class, Mark duplicated classes as deprecated

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/route/MapRouteProgressChangeListener.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/route/MapRouteProgressChangeListener.java
@@ -1,42 +1,45 @@
 package com.mapbox.services.android.navigation.v5.route;
 
-//import android.location.Location;
-//
-//import com.mapbox.services.android.navigation.v5.models.DirectionsRoute;
-//import com.mapbox.services.android.navigation.v5.navigation.NavigationMapRoute;
-//import com.mapbox.services.android.navigation.v5.routeprogress.ProgressChangeListener;
-//import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
-//
-//import java.util.List;
+import android.location.Location;
 
-//public class MapRouteProgressChangeListener implements ProgressChangeListener {
-//
-//  private final NavigationMapRoute mapRoute;
-//
-//  public MapRouteProgressChangeListener(NavigationMapRoute mapRoute) {
-//    this.mapRoute = mapRoute;
-//  }
-//
-//  @Override
-//  public void onProgressChange(Location location, RouteProgress routeProgress) {
-//    DirectionsRoute currentRoute = routeProgress.directionsRoute();
-//    List<DirectionsRoute> directionsRoutes = mapRoute.retrieveDirectionsRoutes();
-//    int primaryRouteIndex = mapRoute.retrievePrimaryRouteIndex();
-//    addNewRoute(currentRoute, directionsRoutes, primaryRouteIndex);
-//    mapRoute.addUpcomingManeuverArrow(routeProgress);
-//    mapRoute.updateRouteLine(location, routeProgress);
-//  }
-//
-//  private void addNewRoute(DirectionsRoute currentRoute, List<DirectionsRoute> directionsRoutes,
-//                           int primaryRouteIndex) {
-//    if (isANewRoute(currentRoute, directionsRoutes, primaryRouteIndex)) {
-//      mapRoute.addRoute(currentRoute);
-//    }
-//  }
-//
-//  private boolean isANewRoute(DirectionsRoute currentRoute, List<DirectionsRoute> directionsRoutes,
-//                              int primaryRouteIndex) {
-//    boolean noRoutes = directionsRoutes.isEmpty();
-//    return noRoutes || !currentRoute.equals(directionsRoutes.get(primaryRouteIndex));
-//  }
-//}
+import com.mapbox.services.android.navigation.v5.models.DirectionsRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.routeprogress.ProgressChangeListener;
+import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
+
+import java.util.List;
+
+/**
+ * @deprecated this class is outdated, use {@link com.mapbox.services.android.navigation.ui.v5.route.MapRouteProgressChangeListener} instead.
+ */
+@Deprecated
+public class MapRouteProgressChangeListener implements ProgressChangeListener {
+
+  private final NavigationMapRoute mapRoute;
+
+  public MapRouteProgressChangeListener(NavigationMapRoute mapRoute) {
+    this.mapRoute = mapRoute;
+  }
+
+  @Override
+  public void onProgressChange(Location location, RouteProgress routeProgress) {
+    DirectionsRoute currentRoute = routeProgress.directionsRoute();
+    List<DirectionsRoute> directionsRoutes = mapRoute.retrieveDirectionsRoutes();
+    int primaryRouteIndex = mapRoute.retrievePrimaryRouteIndex();
+    addNewRoute(currentRoute, directionsRoutes, primaryRouteIndex);
+    mapRoute.addUpcomingManeuverArrow(routeProgress);
+  }
+
+  private void addNewRoute(DirectionsRoute currentRoute, List<DirectionsRoute> directionsRoutes,
+                           int primaryRouteIndex) {
+    if (isANewRoute(currentRoute, directionsRoutes, primaryRouteIndex)) {
+      mapRoute.addRoute(currentRoute);
+    }
+  }
+
+  private boolean isANewRoute(DirectionsRoute currentRoute, List<DirectionsRoute> directionsRoutes,
+                              int primaryRouteIndex) {
+    boolean noRoutes = directionsRoutes.isEmpty();
+    return noRoutes || !currentRoute.equals(directionsRoutes.get(primaryRouteIndex));
+  }
+}
