49a436ec824888699088e98850a7ceb2a33014d4
Fabian Keunecke <f@biankeunecke.de>
2025-04-09 00:28:18 +0200

    Fix `open` class with NSObject not possible, Add docs

--- maplibre-navigation-core/src/iosMain/kotlin/org/maplibre/navigation/core/location/engine/AppleLocationEngine.kt
+++ maplibre-navigation-core/src/iosMain/kotlin/org/maplibre/navigation/core/location/engine/AppleLocationEngine.kt
@@ -16,25 +16,39 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
 /**
- * A [LocationEngine] that uses the Apple CLLocationManager API.
+ * A [LocationEngine] implementation that uses the Apple CLLocationManager API to provide
+ * location updates and fetch the last known location.
  */
-open class AppleLocationEngine : NSObject(), LocationEngine, CLLocationManagerDelegateProtocol {
+open class AppleLocationEngine : LocationEngine {
 
     /**
-     * Underlying CLLocationManager that is used to fetch location and listen to location updates.
+     * A [MutableStateFlow] that holds the current location or null if no location was emitted yet.
      */
-    private val locationManager = CLLocationManager().also { locationManager ->
-        locationManager.delegate = this
-    }
-
     private val locationFlow = MutableStateFlow<Location?>(null)
 
+    /**
+     * The underlying CLLocationManager instance used to fetch location updates.
+     */
+    private val locationManager = CLLocationManager().also { locationManager ->
+        locationManager.delegate = LocationDelegate(locationFlow)
+    }
+
+    /**
+     * Starts listening to location updates based on the provided [request].
+     *
+     * @param request The location request parameters.
+     * @return A [Flow] emitting location updates.
+     */
     override fun listenToLocation(request: LocationEngine.Request): Flow<Location> {
         locationManager.startUpdatingLocation()
-
         return locationFlow.filterNotNull()
     }
 
+    /**
+     * Retrieves the last known location. If no location is available, it fetches the current location.
+     *
+     * @return The last known [Location] or null if unavailable.
+     */
     override suspend fun getLastLocation(): Location? {
         return locationFlow.firstOrNull()
             ?: getLocation()
@@ -43,9 +57,20 @@ open class AppleLocationEngine : NSObject(), LocationEngine, CLLocationManagerDe
                 }
     }
 
+    /**
+     * Suspends the coroutine and fetches the current location using CLLocationManager.
+     *
+     * @return The current [Location] or null if unavailable.
+     */
     private suspend fun getLocation(): Location? = suspendCoroutine { continuation ->
         val locationManager = CLLocationManager()
         locationManager.delegate = object : NSObject(), CLLocationManagerDelegateProtocol {
+            /**
+             * Called when the location manager updates the location.
+             *
+             * @param manager The CLLocationManager instance.
+             * @param didUpdateLocations The list of updated locations.
+             */
             override fun locationManager(
                 manager: CLLocationManager,
                 didUpdateLocations: List<*>
@@ -53,6 +78,12 @@ open class AppleLocationEngine : NSObject(), LocationEngine, CLLocationManagerDe
                 continuation.resume((didUpdateLocations.lastOrNull() as? AppleLocation?)?.toLocation())
             }
 
+            /**
+             * Called when the location manager fails to fetch the location.
+             *
+             * @param manager The CLLocationManager instance.
+             * @param didFailWithError The error that occurred.
+             */
             override fun locationManager(
                 manager: CLLocationManager,
                 didFailWithError: NSError
@@ -64,15 +95,30 @@ open class AppleLocationEngine : NSObject(), LocationEngine, CLLocationManagerDe
         locationManager.requestLocation()
     }
 
-    override fun locationManager(manager: CLLocationManager, didUpdateLocations: List<*>) {
-        (didUpdateLocations.lastOrNull() as? AppleLocation?)
-            ?.toLocation()
-            ?.let { location ->
-                locationFlow.tryEmit(location)
-            }
+    /**
+     * A delegate class for handling location updates and emitting them to the [locationFlow].
+     *
+     * @param locationFlow The [MutableStateFlow] to emit location updates to.
+     */
+    class LocationDelegate(private val locationFlow: MutableStateFlow<Location?>): NSObject(), CLLocationManagerDelegateProtocol {
 
-        if (locationFlow.subscriptionCount.value == 0) {
-            locationManager.stopUpdatingLocation()
+        /**
+         * Called when the location manager updates the location.
+         *
+         * @param manager The CLLocationManager instance.
+         * @param didUpdateLocations The list of updated locations.
+         */
+        override fun locationManager(manager: CLLocationManager, didUpdateLocations: List<*>) {
+            (didUpdateLocations.lastOrNull() as? AppleLocation?)
+                ?.toLocation()
+                ?.let { location ->
+                    locationFlow.tryEmit(location)
+                }
+
+            // Stop updating location if there are no active subscribers.
+            if (locationFlow.subscriptionCount.value == 0) {
+                manager.stopUpdatingLocation()
+            }
         }
     }
 }
